645e439f0fd75cf432affc47e118769b5a0148f9
student <none@none.com>
2015-10-16 20:15:21 -0700

    we in business

---
+++ MUNCH/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  MUNCH
+//
+//  Created by student on 10/16/15.
+//  Copyright (c) 2015 Greybeards. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+- (IBAction)buttonNo:(UIButton *)sender;
+
+@end
+
