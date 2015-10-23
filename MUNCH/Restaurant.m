c7fdf2b1119b76090fcbe2d13371812863f0fbc1
wilcoxch <wilcoxch@sonoma.edu>
2015-10-22 21:08:19 -0700

    Create Restaurant.m

---
+++ MUNCH/Restaurant.m
@@ -0,0 +1,14 @@
+//
+//  NSObject+Restaurant.m
+//  MUNCH
+//
+//  Created by student on 10/22/15.
+//  Copyright (c) 2015 Greybeards. All rights reserved.
+//
+
+#import "Restaurant.h"
+
+@implementation Restaurant{
+}
+
+@end
