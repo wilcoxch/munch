2be740663339e5f410db379209514f88c9a290c1
wilcoxch <wilcoxch@sonoma.edu>
2015-10-22 21:07:53 -0700

    Create Restaurant.h

---
+++ MUNCH/Restaurant.h
@@ -0,0 +1,26 @@
+//
+//  Restaurant.h
+//  MUNCH
+//
+//  Created by student on 10/22/15.
+//  Copyright (c) 2015 Greybeards. All rights reserved.
+//
+
+#ifndef MUNCH_Restaurant_h
+#define MUNCH_Restaurant_h
+#import <UIKit/UIKit.h>
+
+@interface Restaurant : NSObject{
+    
+}
+
+@property NSString *Name;
+@property double *Distance;
+@property NSInteger *Rating;
+@property NSString *Description;
+@property NSString *Address;
+@property NSString *ImgUrl;
+
+@end
+
+#endif
