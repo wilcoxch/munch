d5bcbb4c1ccaed5fcc87481b3d11da75a3feeb8b
Chad Vink <ChadVink@Chads-MacBook-Pro.local>
2015-11-05 16:06:30 -0800

    Got the api to display information

--- MUNCH/ViewController.h
+++ MUNCH/ViewController.h
@@ -8,9 +8,12 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <
+    UITableViewDataSource, UITableViewDelegate>
 
 - (IBAction)buttonNo:(UIButton *)sender;
+@property (strong, nonatomic) IBOutlet UITableView *tableView;
+
 
 @end
 
