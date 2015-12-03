d67e4e88936030aa763b69937dbfe56485ab789a
Chad Vink <ChadVink@Chads-MacBook-Pro.local>
2015-12-02 20:17:36 -0800

    Right swipe has attempted async call, left has just yelp call

--- MUNCH/YPAPISample.m
+++ MUNCH/YPAPISample.m
@@ -13,6 +13,11 @@ static NSString * const kBusinessPath      = @"/v2/business/";
 static NSString * const kSearchLimit       = @"20";
 
 @implementation YPAPISample
+    @synthesize defaultTerm;
+    @synthesize defaultll;
+    @synthesize defaultRadius_filter;
+    @synthesize defaultOffset;
+
 
 #pragma mark - Public
 
@@ -114,6 +119,57 @@ static NSString * const kSearchLimit       = @"20";
 
   NSString *businessPath = [NSString stringWithFormat:@"%@%@", kBusinessPath, businessID];
   return [NSURLRequest requestWithHost:kAPIHost path:businessPath];
+}
+
+- ( NSArray* )YelpCall:(NSString *)offsetNum {
+//    defaultTerm = @"food";
+//    defaultll = @"38.3433588,-122.739119";
+//    defaultRadius_filter = @"30000";
+//    defaultOffset = @"0";
+    
+    // NSString *defaultLocation = @"Rohnert Park, CA";
+    
+    //Get the term and location from the command line if there were any, otherwise assign default values.
+//    NSString *term = [[NSUserDefaults standardUserDefaults] valueForKey:@"term"] ?: defaultTerm;
+//    NSString *ll = [[NSUserDefaults standardUserDefaults] valueForKey:@"ll"] ?: defaultll;
+//    NSString *radius_filter = [[NSUserDefaults standardUserDefaults] valueForKey:@"radius_filter"] ?: defaultRadius_filter;
+//    NSString *offset = [[NSUserDefaults standardUserDefaults] valueForKey:@"offset"] ?: defaultOffset;
+    
+    NSString *term = @"food";
+    NSString *ll = @"38.3433588,-122.739119";
+    NSString *radius_filter = @"30000";
+    NSString *offset = offsetNum;
+    
+    __block NSMutableArray *data;// = [[NSMutableArray alloc] init];
+    YPAPISample *APISample = [[YPAPISample alloc] init];
+    
+    dispatch_group_t requestGroup = dispatch_group_create();
+    
+    dispatch_group_enter(requestGroup);
+    [APISample queryTopBusinessInfoForTerm:term ll:ll radius_filter:radius_filter offset:offset completionHandler:^(NSArray *topBusinessJSON, NSError *error) {
+        
+        if (error) {
+            NSLog(@"An error happened during the request: %@", error);
+        } else if (topBusinessJSON) {
+            NSLog(@"Top business info: \n %@", topBusinessJSON);
+            //NSLog(@"More stuff %@\n", [topBusinessJSON objectForKey:@"categories"]);
+            //NSLog(@"More stuff %lu\n", (unsigned long)[topBusinessJSON count]);
+            //_APIdata = [NSDictionary dictionaryWithDictionary:topBusinessJSON];
+            //NSLog(@"Top business info: \n %@", _APIdata);
+            data = [[NSMutableArray alloc] initWithArray:topBusinessJSON];
+            //            [exampleCardLabels retain];
+            //            for (int i = 0; i < [exampleCardLabels count]; ++i) {
+            //                NSLog(@"Draggable: %@", [exampleCardLabels[i] objectForKey:@"id"]);
+            //            }
+        } else {
+            NSLog(@"No business was found");
+        }
+        
+        dispatch_group_leave(requestGroup);
+    }];
+    
+    dispatch_group_wait(requestGroup, DISPATCH_TIME_FOREVER); // This avoids the program exiting before all our asynchronous callbacks have been made.
+    return data;
 }
 
 @end
